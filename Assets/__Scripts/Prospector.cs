37817da68d8e4d6e0c349f8a3202ba116e4b54a7
RAINBOW\amsea <a.m.searer@gmail.com>
2020-11-17 19:27:58 -0500

    Shuffling the Cards

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -20,5 +20,12 @@ public class Prospector : MonoBehaviour {
     void Start () {
         deck = GetComponent<Deck>(); // Get the Deck
         deck.InitDeck(deckXML.text); // Pass DeckXML to it
+        Deck.Shuffle(ref deck.cards); // This shuffles the deck by reference 
+
+        Card c;
+        for (int cNum=0; cNum<deck.cards.Count; cNum++) {                    // b
+            c = deck.cards[cNum];
+            c.transform.localPosition = new Vector3( (cNum%13)*3, cNum/13*4, 0 );
+        }
     }
 }
